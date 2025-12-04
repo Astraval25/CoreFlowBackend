d93210d746fd29d05d10b1b7176365a34a943f80
Astraval <admin@astraval.com>
2025-12-05 00:14:51 +0530

    Initial commit

---
+++ src/main/java/com/astraval/coreflow/CoreflowApplication.java
@@ -0,0 +1,13 @@
+package com.astraval.coreflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreflowApplication.class, args);
+	}
+
+}
