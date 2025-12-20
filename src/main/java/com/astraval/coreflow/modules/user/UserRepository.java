a44172ff6bc973bcca613d28ec76d5903f57623b
kkbughunter <karthikeyan.aas@gmail.com>
2025-12-20 22:27:47 +0530

    remove commands

--- src/main/java/com/astraval/coreflow/modules/user/UserRepository.java
+++ src/main/java/com/astraval/coreflow/modules/user/UserRepository.java
@@ -10,20 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByEmailAndIsActiveTrue(@Param("email") String email);
-
-    Optional<User> findByUserNameAndIsActiveTrue(@Param("userName") String userName);
-
-    // @Query("""
-    //           SELECT new com.astraval.coreflow.modules.user.dto.AuthRow(
-    //               u.email
-    //           )
-    //           FROM User u
-    //           WHERE u.email = :email
-    //             AND u.isActive = true
-    //         """)
-    // List<AuthRow> findAuthDataByEmail(String email);
     
     @Query("""
                 SELECT u
--- src/main/java/com/astraval/coreflow/modules/user/UserService.java
+++ src/main/java/com/astraval/coreflow/modules/user/UserService.java
@@ -11,21 +11,12 @@ public class UserService {
 
   // Constructor Injection
   private final UserRepository userRepository;
-
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
   @Transactional(readOnly = true)
-  public User findUserByEmail(String email) {
-
-    return userRepository.findByEmailAndIsActiveTrue(email)
-        .orElseThrow(() -> new RuntimeException("User not found: " + email));
-  }
-  
-  @Transactional(readOnly = true)
   public Optional<User> runSelect(String email) {
-    // List<AuthRow> rows = userRepository.findAuthDataByEmail(email);
     return  userRepository.findActiveUserByEmail(email);
   
   }
