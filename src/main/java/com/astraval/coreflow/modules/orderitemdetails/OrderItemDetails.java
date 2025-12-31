8c3e7a90b7ce7bc8324a7e4915b9e0c77084148a
kkbughunter <karthikeyan.aas@gmail.com>
2025-12-31 22:30:07 +0530

    item relation added

--- src/main/java/com/astraval/coreflow/modules/orderitemdetails/OrderItemDetails.java
+++ src/main/java/com/astraval/coreflow/modules/orderitemdetails/OrderItemDetails.java
@@ -2,16 +2,22 @@ package com.astraval.coreflow.modules.orderitemdetails;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.ManyToAny;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.astraval.coreflow.modules.items.Items;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,8 +40,9 @@ public class OrderItemDetails {
   @Column(name = "order_id")
   private Long orderId;
   
-  @Column(name = "item_id")
-  private Long itemId;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id")
+  private Items itemId;
   
   @Column(name = "quantity")
   private Integer quantity;
